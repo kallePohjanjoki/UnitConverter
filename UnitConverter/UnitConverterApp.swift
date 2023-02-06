1f1dc0de151fef4935d3c048d03d318420d18f5a
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2023-02-06 21:43:09 +0200

    Initial commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Kalle Pohjanjoki on 6.2.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
